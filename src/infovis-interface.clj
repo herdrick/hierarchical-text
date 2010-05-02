3d91ece850caafd1223015b021eae2e52809c6c9
herdrick <info@reatlas.com>
2010-05-02 16:49:44 +0800

    put recursively called function into definition of 'controlling' function, 'node' (bad name).  question: how do you do mutual recursion in clojure when there is no 'controlling' function?

--- src/infovis-interface.clj
+++ src/infovis-interface.clj
@@ -6,19 +6,20 @@
 ;}
 
 (defn node [whatever]
+  (defn leaf [file]
+    (str "{id: 'leafnode" (rand) "', name: '" (.replace (str file) "'" "") "', data: {}, children: [] }"))
+
+  (defn pair [two]
+    (str "{id: 'pairnode" (rand) 
+	 "', name: 'summary here"  
+	 "', data: {}" 
+	 ", children: [" 
+	 (node (first two)) 
+	 " , " 
+	 (node (second two)) "] }"))
+
   (if (= "class java.io.File" (str (type whatever)))
     (leaf whatever)
     (pair whatever)))
 
-(defn leaf [file]
-  (str "{id: 'leafnode" (rand) "', name: '" (.replace (str file) "'" "") "', data: {}, children: [] }"))
-
-(defn pair [two]
-  (str "{id: 'pairnode" (rand) 
-       "', name: 'summary here"  
-       "', data: {}" 
-       ", children: [" 
-       (node (first two)) 
-       " , " 
-       (node (second two)) "] }"))
 
