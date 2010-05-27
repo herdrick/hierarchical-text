e49f1c019a9c6d4c69635be2b9e218d8716d01f2
herdrick <info@reatlas.com>
2010-05-26 20:54:37 -0700

    now using Leiningen, and: clojure 1.2.0-master-SNAPSHOT clojure-contrib 1.2.0-SNAPSHOT swank-clojure 1.2.1 incanter 1.2.3-SNAPSHOT

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject hierarchical-classifier "0.5.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+		 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+		 [swank-clojure "1.2.1"]
+		 [incanter "1.2.3-SNAPSHOT"]])
