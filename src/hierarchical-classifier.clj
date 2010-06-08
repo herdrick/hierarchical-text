3a427ea7f717e0663a15b8af130ac4860e108552
herdrick <info@reatlas.com>
2010-06-08 15:27:12 -0700

    refactor: going with pof instead of tree or rfo

--- src/hierarchical-classifier.clj
+++ src/hierarchical-classifier.clj
@@ -7,7 +7,6 @@
 (def file->seq (memoize (fn [file]
 			  (re-seq #"[a-z]+" 
 				  (org.apache.commons.lang.StringUtils/lowerCase (slurp (.toString file)))))))
-
 (defn freqs [words]
   (reduce (fn [freqs obj] 
 	    (merge-with + freqs {obj 1})) 
@@ -41,49 +40,47 @@
 (defn combine-relfreqs [rf1 rf2]
   (merge-with #(mean [% %2]) rf1 rf2))  ; i'm just combining relfreqs taking their (unweighted) mean.  
 
-(def relative-freqs (memoize (fn [tree]
-			       (if (instance? java.io.File tree)
-				 (words->relative-freq (file->seq tree))
-				 (if (= (count tree) 2)
-				   (combine-relfreqs (relative-freqs (first tree)) 
-						     (relative-freqs (second tree)))
-				   (throw (new Error "this should be two trees but isn't=" tree)))))))
+(def relative-freqs (memoize (fn [pof]
+			       (if (instance? java.io.File pof)
+				 (words->relative-freq (file->seq pof))
+				 (if (= (count pof) 2)
+				   (combine-relfreqs (relative-freqs (first pof)) 
+						     (relative-freqs (second pof)))
+				   (throw (new Error "this should be two pofs but isn't=" pof)))))))
  
-(defn interesting-words [tree]
+(defn interesting-words [pof]
   (take *interesting-words-count* (sort #(> (abs (second %)) (abs (second %2)))
 					(map (fn [[word freq]]
-					       [word (- (or (get (relative-freqs tree) word) 0) freq)])
+					       [word (- (or (get (relative-freqs pof) word) 0) freq)])
 					     *corpus-relfreqs*))))
      
-(defn best-pairing [trees word-list omni-relfreq]
-  (let [combos (sort (fn [[first-tree1 first-tree2] [second-tree1 second-tree2]]
-			   (compare (euclid (relative-freqs first-tree1) (relative-freqs first-tree2) word-list)
-				    (euclid (relative-freqs second-tree1) (relative-freqs second-tree2) word-list)))							 
-			 (combinations trees 2))]
+(defn best-pairing [pofs word-list omni-relfreq]
+  (let [combos (sort (fn [[first-pof1 first-pof2] [second-pof1 second-pof2]]
+			   (compare (euclid (relative-freqs first-pof1) (relative-freqs first-pof2) word-list)
+				    (euclid (relative-freqs second-pof1) (relative-freqs second-pof2) word-list)))							 
+			 (combinations pofs 2))]
     (first combos)))
 
-;makes an agglomerative hierarchical cluster of the trees.
-(defn cluster [trees word-list omni-relfreq]
-  (if (= (count trees) 1) 
-    trees ;now it's only one tree
-    (let [best-pairing-tree (best-pairing trees word-list omni-relfreq)
-	  trees-cleaned (filter (complement (fn [tree]
-					     (or (= tree (first best-pairing-tree))
-						 (= tree (second best-pairing-tree)))))
-			       trees)]
-      (cluster (conj trees-cleaned best-pairing-tree) word-list omni-relfreq))))
-
-
+; makes an agglomerative hierarchical cluster of the pofs.
+; pof = pairing or file
+(defn cluster [pofs word-list omni-relfreq]
+  (if (= (count pofs) 1) 
+    pofs ;now it's only one pof
+    (let [best-pairing-pof (best-pairing pofs word-list omni-relfreq)
+	  pofs-cleaned (filter #(not (or (= % (first best-pairing-pof))
+					  (= % (second best-pairing-pof))))
+				pofs)]
+      (cluster (conj pofs-cleaned best-pairing-pof) word-list omni-relfreq))))
 
 
 ;here's how i'm calling this right now:
 ;(def stage-gradual-10 (cluster *docs-rfos* *corpus-word-list* *corpus-relfreqs*))
 ;(.replace (node (first stage-gradual-10)) *directory-string* "")
-;(.replace (node (first (cluster *docs-trees* *corpus-word-list* *standard-relfreq*))) *directory-string* "")
+;(.replace (node (first (cluster *docs-pofs* *corpus-word-list* *standard-relfreq*))) *directory-string* "")
 ;(into-js-file (*1)
-;(map (fn [tree] (filter (complement map?) tree)) (cluster *docs-trees* *corpus-word-list* *corpus-relfreq*))
+;(map (fn [pof] (filter (complement map?) pof)) (cluster *docs-pofs* *corpus-word-list* *corpus-relfreq*))
  
-;(def bazz-4 (cluster *docs-trees* *corpus-word-list* *corpus-relfreqs*))
+;(def bazz-4 (cluster *docs-pofs* *corpus-word-list* *corpus-relfreqs*))
 ;(.replace (node (first bazz-4)) *directory-string* "")
   
 
