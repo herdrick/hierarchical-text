432d221791866db93284d23a63083dc426a9d11c
herdrick <info@reatlas.com>
2010-06-14 14:41:38 -0700

    renamed relative-freq-items to relative-freq-words.  renamed euclid to euclidean.  memoized sort. moved interesting-words.

--- src/hierarchical-classifier.clj
+++ src/hierarchical-classifier.clj
@@ -6,15 +6,16 @@
 (def frequencies-m (memoize frequencies))
 (def count-m (memoize count))
 (def set-m (memoize set))
+(def sort-m (memoize set))
 
 (def to-words (memoize (fn [x]
 			 (cond (coll? x) (apply concat (map to-words x))
 			       true (re-seq #"[a-z]+" (org.apache.commons.lang.StringUtils/lowerCase (slurp (.toString x))))))))
 
-(defn relative-freq-items [xs x]
-  (/ (or (get (frequencies-m xs) x) 0)
-     (count-m xs)))
-
+(def relative-freq-words (memoize (fn [pof word]
+				     (/ (or (get (frequencies-m (to-words pof)) word) 0)
+					(count-m (to-words pof))))))
+     
 (def combine-relfreqs (comp mean vector)) ; i'm just combining relfreqs taking their (unweighted) mean.  
 
 (def relative-freq (memoize (fn [pof word]
@@ -22,23 +23,16 @@
 				(relative-freq-items (to-words pof) word)
 				(combine-relfreqs (relative-freq (first pof) word) 
 						  (relative-freq (second pof) word))))))
-				  
-;euclidean distance
-(defn euclid [pof1 pof2 word-list]
+
+(defn euclidean [pof1 pof2 word-list]
   (sqrt (reduce + (map (fn [word]
 			 (sq (abs (- (relative-freq pof1 word) (relative-freq pof2 word)))))
 		       word-list))))
-
-(defn interesting-words [pof all-files]
-  (sort #(> (abs (second %)) (abs (second %2)))
-	(map (fn [word]
-	       [word (- (relative-freq pof word) (relative-freq-items (to-words all-files) word))])
-	     (set-m (to-words all-files)))))
      
 (defn best-pairing [pofs word-list]
   (first (sort (fn [[pof-1-1 pof-1-2] [pof-2-1 pof-2-2]]
-			   (compare (euclid pof-1-1 pof-1-2 word-list)
-				    (euclid pof-2-1 pof-2-2 word-list)))							 
+			   (compare (euclidean pof-1-1 pof-1-2 word-list)
+				    (euclidean pof-2-1 pof-2-2 word-list)))							 
 			 (combinations pofs 2))))
 
 ; makes an agglomerative hierarchical cluster of the pofs.
@@ -46,9 +40,17 @@
 (defn cluster [pofs]
   (if (= (count pofs) 1) 
     pofs ;now it's only one pof
-    (let [word-list (set-m (to-words (sort (flatten pofs)))) ;sort the result of flatten for more likely cache hit on to-words.
+    (let [word-list (set-m (to-words (sort-m (flatten pofs)))) ;sort the result of flatten for more likely cache hit on to-words.
 	  pofs-cleaned (filter (complement #(some (set [%]) (best-pairing pofs word-list))) pofs)]
       (cluster (conj pofs-cleaned (best-pairing pofs word-list))))))
+
+
+(defn interesting-words [pof all-files]
+  (sort #(> (abs (second %)) (abs (second %2)))
+	(map (fn [word]
+	       [word (- (relative-freq pof word) (relative-freq-words all-files word))])
+	     (set-m (to-words all-files)))))
+
 
 (def directory-string "/Users/herdrick/Dropbox/clojure/hierarchical-classifier/data/mixed")
 (def txt-files (seq (org.apache.commons.io.FileUtils/listFiles (new java.io.File directory-string) nil true)))
