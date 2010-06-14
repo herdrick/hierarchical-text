d26dd4f05d12978a5e655ada8ac8da11aaf0b951
herdrick <info@reatlas.com>
2010-06-14 14:58:02 -0700

    moved word-list best-pairing, where it belongs.

--- src/hierarchical-classifier.clj
+++ src/hierarchical-classifier.clj
@@ -27,28 +27,27 @@
 			 (sq (abs (- (relative-freq pof1 word) (relative-freq pof2 word)))))
 		       word-list))))
      
-(defn best-pairing [pofs word-list]
-  (first (sort (fn [[pof-1-1 pof-1-2] [pof-2-1 pof-2-2]]
-			   (compare (euclidean pof-1-1 pof-1-2 word-list)
-				    (euclidean pof-2-1 pof-2-2 word-list)))							 
-			 (combinations pofs 2))))
+(defn best-pairing [pofs]
+  (let [word-list (set-m (to-words (sort-m (flatten pofs))))] ;sort the result of flatten for more likely cache hit on to-words.
+    (first (sort (fn [[pof-1-1 pof-1-2] [pof-2-1 pof-2-2]]
+		   (compare (euclidean pof-1-1 pof-1-2 word-list)
+			    (euclidean pof-2-1 pof-2-2 word-list)))							 
+		 (combinations pofs 2)))))
 
 ; makes an agglomerative hierarchical cluster of the pofs.
 ; pof = pairing or file
 (defn cluster [pofs]
   (if (= (count pofs) 1) 
     pofs ;now it's only one pof
-    (let [word-list (set-m (to-words (sort-m (flatten pofs)))) ;sort the result of flatten for more likely cache hit on to-words.
-	  pofs-cleaned (filter (complement #(some (set [%]) (best-pairing pofs word-list))) pofs)]
-      (cluster (conj pofs-cleaned (best-pairing pofs word-list))))))
-
+    (cluster (conj (filter (complement #(some (set [%]) (best-pairing pofs)))
+			   pofs)
+		   (best-pairing pofs)))))
 
 (defn interesting-words [pof all-files]
   (sort #(> (abs (second %)) (abs (second %2)))
 	(map (fn [word]
 	       [word (- (relative-freq pof word) (relative-freq-words all-files word))])
 	     (set-m (to-words all-files)))))
-
 
 (def directory-string "/Users/herdrick/Dropbox/clojure/hierarchical-classifier/data/mixed")
 (def txt-files (seq (org.apache.commons.io.FileUtils/listFiles (new java.io.File directory-string) nil true)))
