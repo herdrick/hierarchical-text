5aba973e9c1644030973a9e9f5a0f87f196f9fcf
herdrick <info@reatlas.com>
2010-06-14 20:39:37 -0700

    relative-freq and relative-freq-file now return hashtables (again).  this meant i needed to add merge-general to be able to merge hashtables with various sets of keys.

--- src/hierarchical-classifier.clj
+++ src/hierarchical-classifier.clj
@@ -1,6 +1,7 @@
 (ns user (:use [incanter.core :only (abs sq sqrt)]
 	     [incanter.stats :only (mean)]
-	     [clojure.contrib.combinatorics :only (combinations)]))
+	     [clojure.contrib.combinatorics]
+	     [clojure.set]))
 
 (def frequencies-m (memoize frequencies))
 (def count-m (memoize count))
@@ -11,21 +12,32 @@
 			 (cond (coll? file-or-files) (apply concat (map to-words file-or-files))
 			       true (re-seq #"[a-z]+" (org.apache.commons.lang.StringUtils/lowerCase (slurp (str file-or-files))))))))
 
-(def relative-freq-file (memoize (fn [pof word]
-				     (/ (or (get (frequencies-m (to-words pof)) word) 0)
-					(count-m (to-words pof))))))
-     
-(def relative-freq (memoize (fn [pof word]
+(def relative-freq-file (memoize (fn [pof]
+				   (apply hash-map (flatten (map (fn [[word count]]
+								   [word (/ count (count-m (frequencies-m (to-words pof))))])
+								 (frequencies-m (to-words pof))))))))
+
+(defn merge-general [f g m1 m2]
+  (let [m1-only (difference (set (keys m1)) (set (keys m2)))
+	m2-only (difference (set (keys m2)) (set (keys m1)))]
+    (merge (merge-with f m1 m2)
+	   (into {} (map (fn [k] [k (g (m1 k))]) m1-only))
+	   (into {} (map (fn [k] [k (g (m2 k))]) m2-only)))))
+
+(defn combine-relative-freqs [rf1 rf2]
+  (merge-general (comp mean vector) (fn [val] (mean [val 0])) rf1 rf2))  ; i'm just combining relfreqs taking their (unweighted) mean.  
+
+(def relative-freq (memoize (fn [pof]
 			      (if (instance? java.io.File pof)
-				(relative-freq-file pof word)
-				(mean (vector (relative-freq (first pof) word)  ; combine frequencies by taking their unweighted mean.  
-					      (relative-freq (second pof) word)))))))
-     
+				(relative-freq-file pof)
+				(combine-relative-freqs (relative-freq (first pof))  ; combine frequencies by taking their unweighted mean.  
+							(relative-freq (second pof)))))))
+
 (defn euclidean [pof1 pof2 word-list]
   (sqrt (reduce + (map (fn [word]
-			 (sq (abs (- (relative-freq pof1 word) (relative-freq pof2 word)))))
+			 (sq (abs (- (or ((relative-freq pof1) word) 0) (or ((relative-freq pof2) word) 0)))))
 		       word-list))))
-     
+
 (defn best-pairing [pofs]
   (let [word-list (set-m (to-words (sort-m (flatten pofs))))] ;sort the result of flatten for more likely cache hit on to-words.
     (first (sort (fn [[pof-1-1 pof-1-2] [pof-2-1 pof-2-2]]
@@ -36,7 +48,7 @@
 ; makes an agglomerative hierarchical cluster of the pofs.
 ; pof = pairing or file.  pofs is a list of them.
 (defn cluster [pofs]
-  (if (= (count pofs) 1) 
+  (if (= (count pofs) 1)
     pofs
     (cluster (conj (filter (complement #(some (set [%]) (best-pairing pofs)))
 			   pofs)
@@ -45,7 +57,7 @@
 (defn interesting-words [pof all-files]
   (sort #(> (abs (second %)) (abs (second %2)))
 	(map (fn [word]
-	       [word (- (relative-freq pof word) (relative-freq-file all-files word))])
+	       [word (- (or ((relative-freq pof) word) 0) (or ((relative-freq-file all-files) word) 0))])
 	     (set-m (to-words all-files)))))
 
 (def directory-string "/Users/herdrick/Dropbox/clojure/hierarchical-classifier/data/mixed")
