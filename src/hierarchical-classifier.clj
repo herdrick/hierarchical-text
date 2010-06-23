0f7bf99f6e5ceb1eec4677a5616793cbe11fb3c4
herdrick <info@reatlas.com>
2010-06-22 17:39:34 -0700

    pof->js instead of node as the public fn.  axed superfluous abs.  added word-list fn.  renamed relative-freq-file to relative-freq-files.

--- src/hierarchical-classifier.clj
+++ src/hierarchical-classifier.clj
@@ -1,52 +1,56 @@
-(ns user (:use [incanter.core :only (abs sq sqrt)]
-	     [incanter.stats :only (mean)]
-	     [clojure.contrib.combinatorics :only (combinations)]))
+(ns radical (:use [incanter.core :only (abs sq sqrt)]
+		  [incanter.stats :only (mean)]
+		  [clojure.contrib.combinatorics :only (combinations)]))
 
 (def frequencies-m (memoize frequencies))
 (def count-m (memoize count))
 (def set-m (memoize set))
-(def sort-m (memoize set))
+(def flatten-m (memoize flatten))
 
 (def to-words (memoize (fn [file-or-files]
-			 (cond (coll? file-or-files) (apply concat (map to-words file-or-files))
-			       true (re-seq #"[a-z]+" (org.apache.commons.lang.StringUtils/lowerCase (slurp (str file-or-files))))))))
+			 (if (coll? file-or-files)
+			   (apply concat (map to-words (set-m (flatten-m file-or-files))))
+			   (re-seq #"[a-z]+" (org.apache.commons.lang.StringUtils/lowerCase (slurp (str file-or-files))))))))
 
-(def relative-freq-file (memoize (fn [pof word]
+(def word-list (memoize (fn [pof]
+			  (set-m (to-words pof)))))
+				 
+(def relative-freq-files (memoize (fn [pof word]
 				     (/ (or (get (frequencies-m (to-words pof)) word) 0)
 					(count-m (to-words pof))))))
      
 (def relative-freq (memoize (fn [pof word]
 			      (if (instance? java.io.File pof)
-				(relative-freq-file pof word)
+				(relative-freq-files pof word)
 				(mean (vector (relative-freq (first pof) word)  ; combine frequencies by taking their unweighted mean.  
 					      (relative-freq (second pof) word)))))))
      
 (def euclidean (memoize (fn [pof1 pof2 word-list]
 			  (sqrt (reduce + (map (fn [word]
-						 (sq (abs (- (relative-freq pof1 word) (relative-freq pof2 word)))))
+						 (sq (- (relative-freq pof1 word) (relative-freq pof2 word))))
 					       word-list))))))
      
 (defn best-pairing [pofs]
-  (let [word-list (set-m (to-words (sort-m (flatten pofs))))] ;sort the result of flatten for more likely cache hit on to-words.
+  (let [words (word-list pofs)] ;let clause for brevity  ;sort the result of flatten for more likely cache hit on to-words.
     (first (sort (fn [[pof-1-1 pof-1-2] [pof-2-1 pof-2-2]]
-		   (compare (euclidean pof-1-1 pof-1-2 word-list)
-			    (euclidean pof-2-1 pof-2-2 word-list)))							 
+		   (compare (euclidean pof-1-1 pof-1-2 words)
+			    (euclidean pof-2-1 pof-2-2 words)))							 
 		 (combinations pofs 2)))))
 
 ; makes an agglomerative hierarchical cluster of the pofs.
 ; pof = pairing or file.  pofs is a list of them.
 (defn cluster [pofs]
   (if (= (count pofs) 1) 
-    pofs
+    (first pofs)
     (cluster (conj (filter (complement #(some (set [%]) (best-pairing pofs)))
 			   pofs)
 		   (best-pairing pofs)))))
 
-(defn interesting-words [pof all-files]
+(defn interesting-words [pof top-pof]
   (sort #(> (abs (second %)) (abs (second %2)))
 	(map (fn [word]
-	       [word (- (relative-freq pof word) (relative-freq-file all-files word))])
-	     (set-m (to-words all-files)))))
+	       [word (- (relative-freq pof word) (relative-freq-files top-pof word))])
+	     (word-list top-pof))))
 
-(def directory-string "/Users/herdrick/Dropbox/clojure/hierarchical-classifier/data/mixed")
-(def txt-files (seq (org.apache.commons.io.FileUtils/listFiles (new java.io.File directory-string) nil true)))
+(def *directory-string* "/Users/herdrick/Dropbox/clojure/hierarchical-classifier/data/store/three-file-stash/")
+(def *txt-files* (seq (org.apache.commons.io.FileUtils/listFiles (new java.io.File *directory-string*) nil false)))
--- src/protovis-interface.clj
+++ src/protovis-interface.clj
@@ -1,29 +1,33 @@
-(ns user)
-(defn node [whatever interesting-words-count all-files]
-  (defn leaf [pof]
-    (str "'" (.replace (str pof) "'" "") "'"  
-	 " : 1")) 
-  (defn pair [pof]
-    (str "'"
-	 (apply str (.trim (apply str  
-				  (map (fn [[word freq]]
-					 (let [left-wrapper (if (< freq 0) "(" "")
-					       right-wrapper (if (< freq 0) ")" "")]
-					   (str left-wrapper (.trim word) right-wrapper " ")))
-				       (take interesting-words-count (interesting-words pof all-files)))))) 
-	 "': {"   
-	 (node (first pof) interesting-words-count all-files)
-	 " , " 
-	 (node (second pof) interesting-words-count all-files) "}"))
-   
-  (if (instance? java.io.File whatever)
-    (leaf whatever)
-    (pair whatever))) 
+(ns radical)
 
+(defn pof->js [top-pof interesting-words-count]
+  (defn node [pof]
+    (defn leaf [pof]
+      (prn "leaf " pof)
+      (str "'" (.replace (str pof) "'" "") "'"  
+	   " : 1")) 
+    (defn pair [pof]
+      (prn "pair " pof)
+      (str "'"
+	   (apply str (.trim (apply str  
+				    (map (fn [[word freq]]
+					   (let [left-wrapper (if (< freq 0) "(" "")
+						 right-wrapper (if (< freq 0) ")" "")]
+					     (str left-wrapper (.trim word) right-wrapper " ")))
+					 (take interesting-words-count (interesting-words pof top-pof)))))) 
+	   "': {"   
+	   (node (first pof))
+	   " , " 
+	   (node (second pof)) "}"))
+    (prn "node " pof)
+    (if (instance? java.io.File pof)
+      (leaf pof)
+      (pair pof)))
+  (node top-pof))
+  
 (def *protovis-json-file* "file:///Users/herdrick/Dropbox/clojure/hierarchical-classifier/visualize/protovis-3.2/flare.js")
 
 (defn write-protovis-file [o]
   (spit *protovis-json-file* 
 	(str "var flare = {" o "};")))
-
 
