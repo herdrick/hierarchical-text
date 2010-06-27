743310a7d88f71c167f43c9b88c038d75412f60c
herdrick <info@reatlas.com>
2010-06-26 19:17:15 -0700

    whitespace

--- src/protovis-interface.clj
+++ src/protovis-interface.clj
@@ -2,30 +2,30 @@
 (defn pof->js [top-pof interesting-words-count]
   (let [all-files (flatten top-pof)
 	corpus-freqs (freq-files all-files)]	
-  (defn node [pof]
-    (defn leaf [pof]
-      (str "'" (.replace (str pof) "'" "") "'"  
-	   " : 1"))
+    (defn node [pof]
+      (defn leaf [pof]
+	(str "'" (.replace (str pof) "'" "") "'"  
+	     " : 1"))
+      
+      (defn pair [pof]
+	(str "'"
+	     (apply str (.trim (apply str  
+				      (map (fn [[word freq]]
+					     (let [left-wrapper (if (< freq 0) "(" "")
+						   right-wrapper (if (< freq 0) ")" "")]
+					       (str left-wrapper (.trim word) right-wrapper " ")))
+					   (take interesting-words-count (interesting-words pof corpus-freqs)))))) 
+	     "': {"   
+	     (node (first pof))
+	     " , " 
+	     (node (second pof)) "}"))
+      
+      (if (instance? java.io.File pof)
+	(leaf pof)
+	(pair pof)))
     
-    (defn pair [pof]
-      (str "'"
-	   (apply str (.trim (apply str  
-				  (map (fn [[word freq]]
-					 (let [left-wrapper (if (< freq 0) "(" "")
-					       right-wrapper (if (< freq 0) ")" "")]
-					   (str left-wrapper (.trim word) right-wrapper " ")))
-				       (take interesting-words-count (interesting-words pof corpus-freqs)))))) 
-	   "': {"   
-	   (node (first pof))
-	   " , " 
-	   (node (second pof)) "}"))
-    
-    (if (instance? java.io.File pof)
-      (leaf pof)
-      (pair pof)))
-  
-  (node top-pof)))
-  
+    (node top-pof)))
+
 (def *protovis-json-file* "file:///Users/herdrick/Dropbox/clojure/hierarchical-classifier/visualize/protovis-3.2/flare.js")
 
 (defn write-protovis-file [o]
